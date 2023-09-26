8c97b81d68233802eb38502447ff4dd1bbbb2d85
IlyaBokovv <ilyusha215@gmail.com>
2023-09-26 23:39:58 +0300

    Actors + cinema views and controllers

---
+++ MovieLibraryWeb/Controllers/ActorsController.cs
@@ -0,0 +1,103 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using MovieLibrary.Models.Models;
+using MovieLibrary.Models.Static;
+using MovieLibrary.Models.ViewModels;
+using Services.Interfaces;
+using System.Data;
+
+namespace MovieLibraryWeb.Controllers
+{
+    [Authorize(Roles = UserRolesValues.Admin)]
+    public class ActorsController : Controller
+    {
+        private readonly IActorService _actorService;
+        private readonly IWebHostEnvironment _hostEnvironment;
+
+        public ActorsController(IActorService actorService, IWebHostEnvironment hostEnvironment)
+        {
+            this._actorService = actorService;
+            this._hostEnvironment = hostEnvironment;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var actors = await _actorService.GetAllAsync(trackChanges: false, a => a.Image);
+            return View(actors);
+        }
+
+        public IActionResult Create()
+        {
+            return View(new ActorVM());
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(ActorVM actorVM)
+        {
+            var actor = new Actor()
+            {
+                FullName = actorVM.FullName,
+                Bio = actorVM.Bio,
+                Image = new Image() { ImageFile = actorVM.Image.ImageFile }
+            };
+            if (!ModelState.IsValid)
+            {
+                return View(actor);
+            }
+            await _actorService.AddActorWithImageUplodaing(actor);
+            return RedirectToAction(nameof(Index));
+        }
+        [AllowAnonymous]
+        public async Task<IActionResult> Details(int id)
+        {
+            var actor = await _actorService.GetByIdAsync(id, trackChanges: false, a => a.Image);
+            if (actor == null)
+            {
+                return View("NotFound");
+            }
+            return View(actor);
+        }
+
+
+        public async Task<IActionResult> Edit(int id)
+        {
+            var actor = await _actorService.GetByIdAsync(id, trackChanges: false, a => a.Image);
+            if (actor == null)
+            {
+                return View("NotFound");
+            }
+            return View(actor);
+        }
+        [HttpPost]
+        public async Task<IActionResult> Edit(Actor actor)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(actor);
+            }
+
+            await _actorService.UpdateActorWithImageAsync(actor);
+            return RedirectToAction(nameof(Index));
+        }
+        public async Task<IActionResult> Delete(int id)
+        {
+            var actor = await _actorService.GetByIdAsync(id, trackChanges: false, a => a.Image);
+            if (actor == null)
+            {
+                return View("NotFound");
+            }
+            return View(actor);
+        }
+        [HttpPost]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var actor = await _actorService.GetByIdAsync(id, trackChanges: false, a => a.Image);
+            if (actor == null)
+            {
+                return View("NotFound");
+            }
+            await _actorService.DeleteAsyncWithImage(actor);
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
---
+++ MovieLibraryWeb/Controllers/CinemasController.cs
@@ -0,0 +1,104 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using MovieLibrary.Models.Models;
+using MovieLibrary.Models.Static;
+using MovieLibrary.Models.ViewModels;
+using MovieLibrary.Services.Interfaces;
+
+namespace mvc.Controllers
+{
+    [Authorize(Roles = UserRolesValues.Admin)]
+    public class CinemasController : Controller
+    {
+        private readonly ICinemaService _cinemaService;
+
+        public CinemasController(ICinemaService cinemaService)
+        {
+            this._cinemaService = cinemaService;
+        }
+        public async Task<IActionResult> Index()
+        {
+            var cinemas = await _cinemaService.GetAllAsync(trackChanges: false, d => d.Image);
+            return View(cinemas);
+        }
+        [AllowAnonymous]
+        // GET: Cinemas/Details/{id}
+        public async Task<IActionResult> Details(int id)
+        {
+            var cinema = await _cinemaService.GetByIdAsync(id, trackChanges: false, d => d.Image);
+            if (cinema != null)
+            {
+                return View(cinema);
+            }
+            return View("NotFound");
+        }
+
+        // Get: Cinemas/Create
+        public IActionResult Create()
+        {
+            return View(new CinemaVM());
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(CinemaVM cinemaVM)
+        {
+            var cinema = new Cinema
+            {
+                Name = cinemaVM.Name,
+                Description = cinemaVM.Description,
+                Image = new Image() { ImageFile = cinemaVM.Image.ImageFile }
+            };
+            if (!ModelState.IsValid)
+            {
+                return View(cinema);
+            }
+            await _cinemaService.AddWithImageUplodaing(cinema);
+            return RedirectToAction(nameof(Index));
+        }
+
+        // GET: Cinemas/Edit/{id}
+        public async Task<IActionResult> Edit(int id)
+        {
+            var cinema = await _cinemaService.GetByIdAsync(id, trackChanges: false, d => d.Image);
+            if (cinema == null)
+            {
+                return View("NotFound");
+            }
+            return View(cinema);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Edit(Cinema cinema)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(cinema);
+            }
+            await _cinemaService.UpdateWithImageAsync(cinema);
+            return RedirectToAction(nameof(Index));
+        }
+
+        // GET: Cinemas/Delete/{id}
+        public async Task<IActionResult> Delete(int id)
+        {
+            var cinema = await _cinemaService.GetByIdAsync(id, trackChanges: false, d => d.Image);
+            if (cinema == null)
+            {
+                return View("NotFound");
+            }
+            return View(cinema);
+        }
+        [HttpPost]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var cinema = await _cinemaService.GetByIdAsync(id, trackChanges: false, d => d.Image);
+            if (cinema == null)
+            {
+                return View("NotFound");
+            }
+            await _cinemaService.DeleteAsyncWithImage(cinema);
+            return RedirectToAction(nameof(Index));
+        }
+
+    }
+}
