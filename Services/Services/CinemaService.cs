6a75195c432d039132f1e0a7f41683e73048fc34
IlyaBokovv <ilyusha215@gmail.com>
2023-10-03 02:17:00 +0300

    Minor cinema service fix

--- Services/Services/CinemaService.cs
+++ Services/Services/CinemaService.cs
@@ -10,17 +10,6 @@ namespace MovieLibrary.Services.Services
     {
         private readonly ApplicationDbContext _db;
         private readonly IImageUploadService _imageUploadService;
-        private static Dictionary<char, string> transliterationTable = new Dictionary<char, string>
-            {
-        {'а', "a"}, {'б', "b"}, {'в', "v"}, {'г', "g"}, {'д', "d"}, {'е', "e"}, {'ё', "yo"}, {'ж', "zh"}, {'з', "z"},
-        {'и', "i"}, {'й', "y"}, {'к', "k"}, {'л', "l"}, {'м', "m"}, {'н', "n"}, {'о', "o"}, {'п', "p"}, {'р', "r"},
-        {'с', "s"}, {'т', "t"}, {'у', "u"}, {'ф', "f"}, {'х', "kh"}, {'ц', "ts"}, {'ч', "ch"}, {'ш', "sh"},
-        {'щ', "shch"}, {'ъ', ""}, {'ы', "y"}, {'ь', ""}, {'э', "e"}, {'ю', "yu"}, {'я', "ya"},
-        {'А', "A"}, {'Б', "B"}, {'В', "V"}, {'Г', "G"}, {'Д', "D"}, {'Е', "E"}, {'Ё', "Yo"}, {'Ж', "Zh"}, {'З', "Z"},
-        {'И', "I"}, {'Й', "Y"}, {'К', "K"}, {'Л', "L"}, {'М', "M"}, {'Н', "N"}, {'О', "O"}, {'П', "P"}, {'Р', "R"},
-        {'С', "S"}, {'Т', "T"}, {'У', "U"}, {'Ф', "F"}, {'Х', "Kh"}, {'Ц', "Ts"}, {'Ч', "Ch"}, {'Ш', "Sh"},
-        {'Щ', "Shch"}, {'Ъ', ""}, {'Ы', "Y"}, {'Ь', ""}, {'Э', "E"}, {'Ю', "Yu"}, {'Я', "Ya"}
-            };
 
         public CinemaService(ApplicationDbContext db, IImageUploadService imageUploadService) : base(db)
         {
@@ -46,7 +35,7 @@ namespace MovieLibrary.Services.Services
             }
             _imageUploadService.Delete(oldImage.ImagePath);
 
-            var imagePath = await _imageUploadService.UploadAsync(cinema.Image, nameof(Cinema) + ConvertToTranslit(cinema.Name!));
+            var imagePath = await _imageUploadService.UploadAsync(cinema.Image, nameof(Cinema) + CinemaServiceHelpers.ConvertToTranslit(cinema.Name!));
             cinema.Image.ImagePath = imagePath;
 
 
@@ -63,7 +52,7 @@ namespace MovieLibrary.Services.Services
         }
         public async Task<Cinema> AddWithImageUplodaing(Cinema cinema)
         {
-            var imagePath = await _imageUploadService.UploadAsync(cinema.Image, nameof(Cinema) + ConvertToTranslit(cinema.Name!));
+            var imagePath = await _imageUploadService.UploadAsync(cinema.Image, nameof(Cinema) + CinemaServiceHelpers.ConvertToTranslit(cinema.Name!));
 
             cinema.Image.ImagePath = imagePath;
             await _db.Cinemas.AddAsync(cinema);
@@ -81,21 +70,7 @@ namespace MovieLibrary.Services.Services
             await _db.SaveChangesAsync();
         }
 
-        public static string ConvertToTranslit(string input)
-        {
-            char[] inputChars = input.ToCharArray();
-            List<string> outputChars = new List<string>();
 
-            foreach (char c in inputChars)
-            {
-                if (transliterationTable.ContainsKey(c))
-                    outputChars.Add(transliterationTable[c]);
-                else
-                    outputChars.Add(c.ToString());
-            }
-
-            return string.Join("", outputChars);
-        }
 
     }
 }
