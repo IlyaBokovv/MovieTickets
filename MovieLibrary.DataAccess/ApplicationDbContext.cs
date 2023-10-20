5b73701e7711e5ecfedb0ede042a16fcf05d43e6
IlyaBokovv <ilyusha215@gmail.com>
2023-10-20 06:56:11 +0300

    DbContext refactor

--- MovieLibrary.DataAccess/ApplicationDbContext.cs
+++ MovieLibrary.DataAccess/ApplicationDbContext.cs
@@ -8,16 +8,16 @@ namespace MovieLibrary.DataAccess
 {
     public class ApplicationDbContext : IdentityDbContext<AppUser>
     {
-        public DbSet<Actor> Actors { get; set; }
-        public DbSet<Movie> Movies { get; set; }
-        public DbSet<ActorMovie> ActorMovies { get; set; }
-        public DbSet<Director> Directors { get; set; }
-        public DbSet<Cinema> Cinemas { get; set; }
-        public DbSet<Order> Orders { get; set; }
-        public DbSet<OrderItem> OrderItems { get; set; }
-        public DbSet<Cart> Carts { get; set; }
-        public DbSet<CartItem> CartItems { get; set; }
-        public DbSet<Image> Images { get; set; }
+        public DbSet<Actor> Actors => Set<Actor>();
+        public DbSet<Movie> Movies => Set<Movie>();
+        public DbSet<ActorMovie> ActorMovies => Set<ActorMovie>();
+        public DbSet<Director> Directors => Set<Director>();
+        public DbSet<Cinema> Cinemas => Set<Cinema>();
+        public DbSet<Order> Orders => Set<Order>();
+        public DbSet<OrderItem> OrderItems => Set<OrderItem>();
+        public DbSet<Cart> Carts => Set<Cart>();
+        public DbSet<CartItem> CartItems => Set<CartItem>();
+        public DbSet<Image> Images => Set<Image>();
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
         {
             Database.Migrate();
@@ -25,17 +25,7 @@ namespace MovieLibrary.DataAccess
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
-            modelBuilder.ApplyConfiguration(new ActorConfiguirations());
-            modelBuilder.ApplyConfiguration(new ActorsMoviesConfigurations());
-            modelBuilder.ApplyConfiguration(new MovieConfigurations());
-            modelBuilder.ApplyConfiguration(new CinemaConfigurations());
-            modelBuilder.ApplyConfiguration(new DirectorConfigurations());
-            modelBuilder.ApplyConfiguration(new OrderConfigurations());
-            modelBuilder.ApplyConfiguration(new OrderItemConfigurations());
-            modelBuilder.ApplyConfiguration(new CartConfigurations());
-            modelBuilder.ApplyConfiguration(new CartItemConfigurations());
-            modelBuilder.ApplyConfiguration(new ImageConfigurations());
-            modelBuilder.ApplyConfiguration(new UserAddressConfigurations());
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(ApplicationDbContext).Assembly);
             base.OnModelCreating(modelBuilder);
         }
     }
